ce530d3557dd9d9fc5ba2c49fb4d19a84b172aa0
Cailin Young <cailinfayyoung@gmail.com>
2021-02-06 18:58:15 -0700

    setting up app.js file framework

--- app.js
+++ app.js
@@ -36,18 +36,69 @@ const render = require("./lib/htmlRenderer");
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
 
-function init() {
-    inquirer.prompt([
-        /* Pass your questions in here */
-    ])
-        .then(answers => {
-            // Use user feedback for... whatever!!
-        })
-        .catch(error => {
-            if (error.isTtyError) {
-                // Prompt couldn't be rendered in the current environment
-            } else {
-                // Something else went wrong
-            }
-        });
+function teamGenerator (){
+
+    function addManager() {
+        inquirer.prompt([
+        ]
+        ])
+            .then(answers => {
+                // Use user feedback for... whatever!!
+            })
+            .catch(error => {
+                if (error.isTtyError) {
+                    // Prompt couldn't be rendered in the current environment
+                } else {
+                    // Something else went wrong
+                }
+            });
+    }
+    
+    function init() {
+        inquirer.prompt([
+            /* Pass your questions in here */
+        ])
+            .then(answers => {
+                // Use user feedback for... whatever!!
+            })
+            .catch(error => {
+                if (error.isTtyError) {
+                    // Prompt couldn't be rendered in the current environment
+                } else {
+                    // Something else went wrong
+                }
+            });
+    }
+    
+    function init() {
+        inquirer.prompt([
+            /* Pass your questions in here */
+        ])
+            .then(answers => {
+                // Use user feedback for... whatever!!
+            })
+            .catch(error => {
+                if (error.isTtyError) {
+                    // Prompt couldn't be rendered in the current environment
+                } else {
+                    // Something else went wrong
+                }
+            });
+    }
+    
+    function init() {
+        inquirer.prompt([
+            /* Pass your questions in here */
+        ])
+            .then(answers => {
+                // Use user feedback for... whatever!!
+            })
+            .catch(error => {
+                if (error.isTtyError) {
+                    // Prompt couldn't be rendered in the current environment
+                } else {
+                    // Something else went wrong
+                }
+            });
+    }
 }
